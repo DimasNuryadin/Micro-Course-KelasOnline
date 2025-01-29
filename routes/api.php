def93aceb26e30244e3c7cd396e6eaacf5c041bf
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2025-01-29 14:45:51 +0700

    done create API MyCourse

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\ImageCourseController;
 use App\Http\Controllers\LessonController;
+use App\Http\Controllers\MyCourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -49,3 +50,7 @@ Route::delete('/lessons/{id}', [LessonController::class, 'destroy']);
 // Image Course
 Route::post('/image-courses', [ImageCourseController::class, 'create']);
 Route::delete('/image-courses/{id}', [ImageCourseController::class, 'destroy']);
+
+// My Courses
+Route::post('/my-courses', [MyCourseController::class, 'create']);
+Route::get('/my-courses', [MyCourseController::class, 'index']);
